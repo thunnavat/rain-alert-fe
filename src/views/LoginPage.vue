cc5ef8b73bc533c8b3930a72398ccea07d27ca9d
thunnavat <thunnavat.c@mail.kmutt.ac.th>
2024-02-11 18:21:12 +0700

    feat: line login button

---
+++ src/views/LoginPage.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+  <div>
+    <button class="bg-[#06C755] flex items-center justify-items-center py-1">
+        <img src="../assets/LineIcon.png" alt="LineIcon" class="w-10 h-10 mr-3" />
+        <span class="pr-2">Log in with LINE</span>
+    </button>
+  </div>
+</template>
+
+<style scoped></style>
