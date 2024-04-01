f83abd53acf391b213c1c4e40b1fbc7f07c1f406
JOJ0123 <wisarut.jojo@mail.kmutt.ac.th>
2024-04-01 22:00:08 +0700

    fix: remove unused vite config

--- vite.config.js
+++ vite.config.js
@@ -7,17 +7,6 @@ import pluginRewriteAll from 'vite-plugin-rewrite-all';
 export default defineConfig({
   plugins: [vue(), pluginRewriteAll()],
   base: "/tt3",
-  dev: {
-    proxyTable: {
-      '/forget-password/*.*': { // this will match all urls with dots after '/t/'
-        target: 'http://localhost:8080/',  // send to webpack dev server
-        router: function (req) {
-          req.url = 'index.html'  // Send to vue app
-        }
-      }
-      // Any other routes you need to bypass should go here.
-    }
-    },
   server: {
     proxy: {
       '/api': {
